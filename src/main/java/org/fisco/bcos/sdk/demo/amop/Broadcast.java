3f92fed1948a7868f9120a72da4df0530bb5f651
octopus <912554887@qq.com>
2021-11-10 08:26:12 +0800

    add amop test

---
+++ src/main/java/org/fisco/bcos/sdk/demo/amop/Broadcast.java
@@ -0,0 +1,54 @@
+package org.fisco.bcos.sdk.demo.amop;
+
+import java.net.URL;
+import org.fisco.bcos.sdk.amop.Amop;
+import org.fisco.bcos.sdk.config.Config;
+import org.fisco.bcos.sdk.config.ConfigOption;
+import org.fisco.bcos.sdk.config.exceptions.ConfigException;
+import org.fisco.bcos.sdk.demo.perf.ParallelOkPerf;
+import org.fisco.bcos.sdk.jni.common.JniException;
+import org.fisco.bcos.sdk.model.ConstantConfig;
+
+public class Broadcast {
+    public static void usage() {
+        System.out.println("\tUsage: ");
+        System.out.println(
+                "\t\tjava -cp \"conf/:lib/*:apps/*\"  org.fisco.bcos.sdk.demo.amop.Broadcast topic msg");
+        System.out.println("\tExample:");
+        System.out.println(
+                "\t\tjava -cp \"conf/:lib/*:apps/*\"  org.fisco.bcos.sdk.demo.amop.Broadcast topic HelloWorld");
+        System.exit(0);
+    }
+
+    public static void main(String[] args)
+            throws InterruptedException, JniException, ConfigException {
+
+        String configFileName = ConstantConfig.CONFIG_FILE_NAME;
+        URL configUrl = ParallelOkPerf.class.getClassLoader().getResource(configFileName);
+        if (configUrl == null) {
+            System.out.println("The configFile " + configFileName + " doesn't exist!");
+            return;
+        }
+
+        if (args.length < 2) {
+            usage();
+        }
+
+        String topic = args[0];
+        String msg = args[1];
+
+        System.out.println(" ====== AMOP broadcast, topic: " + topic + " ,msg: " + msg);
+
+        String configFile = configUrl.getPath();
+        ConfigOption configOption = Config.load(configFile);
+
+        org.fisco.bcos.sdk.amop.Amop amop = Amop.build(configOption);
+        amop.start();
+
+        while (true) {
+            System.out.println(" ==>  try to broadcast message: " + msg);
+            amop.broadcastAmopMsg(topic, msg.getBytes());
+            Thread.sleep(10000);
+        }
+    }
+}
