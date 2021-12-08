5da3bd910a9a46ea33f37d24e871a05d2a24a1a7
morebtcg <651932351@qq.com>
2021-12-08 12:53:22 +0800

    Disable dag flag

--- src/main/java/org/fisco/bcos/sdk/demo/perf/ParallelOkPerf.java
+++ src/main/java/org/fisco/bcos/sdk/demo/perf/ParallelOkPerf.java
@@ -67,9 +67,10 @@ public class ParallelOkPerf {
             String configFile = configUrl.getPath();
             BcosSDK sdk = BcosSDK.build(configFile);
             client = sdk.getClient(groupId);
-            client.setDAG(true);
             dagUserInfo.setFile(userFile);
-            ThreadPoolService threadPoolService = new ThreadPoolService("ParallelOkPerf", 1000000);
+            ThreadPoolService threadPoolService =
+                    new ThreadPoolService(
+                            "ParallelOkPerf", Runtime.getRuntime().availableProcessors());
 
             if (perfType.compareToIgnoreCase("parallelok") == 0) {
                 parallelOkPerf(groupId, command, count, qps, threadPoolService);
