15ad764859690508c4933f1c47050814bbade329
cyjseagull <1547158861@qq.com>
2022-08-10 23:03:18 +0800

    adapt SM2EvidenceVerify (#123)

--- src/main/java/org/fisco/bcos/sdk/demo/contract/sol/Evidence.sol
+++ src/main/java/org/fisco/bcos/sdk/demo/contract/sol/Evidence.sol
@@ -1,23 +1,24 @@
-pragma solidity ^0.4.4;
+pragma solidity >=0.6.10 <0.8.20;
+pragma experimental ABIEncoderV2;
 contract Evidence{
     string evidence;
     string evidenceInfo;
     string evidenceId;
     event newEvidenceEvent(string evi, string info, string id);
-    function Evidence(string evi, string info, string id) public 
+    constructor(string memory evi, string memory info, string memory id) public 
     {
         evidence = evi;
         evidenceInfo = info;
         evidenceId = id;
-        newEvidenceEvent(evi,info,id);
+        emit newEvidenceEvent(evi,info,id);
     }
 
-    function getEvidenceInfo() public constant returns(string)
+    function getEvidenceInfo() public view returns(string memory)
     {
         return evidenceInfo;
     }
 
-    function getEvidence() public constant returns(string,string,string){
+    function getEvidence() public view returns(string memory,string memory,string memory){
         return(evidence,evidenceInfo,evidenceId);
     }
 }
