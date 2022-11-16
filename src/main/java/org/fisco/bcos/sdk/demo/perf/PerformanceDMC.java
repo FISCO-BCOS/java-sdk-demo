c28dbf24618c1fcc237e625f9ed8ab8a078ea0c6
monan <651932351@qq.com>
2022-11-16 10:12:42 +0800

    Update perf program

--- src/main/java/org/fisco/bcos/sdk/demo/perf/PerformanceDMC.java
+++ src/main/java/org/fisco/bcos/sdk/demo/perf/PerformanceDMC.java
@@ -17,12 +17,12 @@ import com.google.common.util.concurrent.RateLimiter;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.net.URL;
-import java.util.Map;
 import java.util.Random;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.IntStream;
+
 import me.tongfei.progressbar.ProgressBar;
 import me.tongfei.progressbar.ProgressBarBuilder;
 import me.tongfei.progressbar.ProgressBarStyle;
@@ -35,207 +35,144 @@ import org.fisco.bcos.sdk.v3.model.callback.TransactionCallback;
 import org.fisco.bcos.sdk.v3.transaction.model.exception.ContractException;
 import org.fisco.bcos.sdk.v3.utils.ThreadPoolService;
 
+/**
+ * 
+ * @author monan
+ *
+ */
 public class PerformanceDMC {
-    private static Client client;
+	private static Client client;
 
-    public static void Usage() {
-        System.out.println(" Usage:");
-        System.out.println("===== PerformanceDMC test===========");
-        System.out.println(
-                " \t java -cp 'conf/:lib/*:apps/*' org.fisco.bcos.sdk.demo.perf.PerformanceDMC [groupId] [userCount] [count] [qps].");
-    }
+	public static void usage() {
+		System.out.println(" Usage:");
+		System.out.println("===== PerformanceDMC test===========");
+		System.out.println(
+				" \t java -cp 'conf/:lib/*:apps/*' org.fisco.bcos.sdk.demo.perf.PerformanceDMC [groupId] [userCount] [count] [qps].");
+	}
 
-    public static void main(String[] args)
-            throws ContractException, IOException, InterruptedException {
-        try {
-            String configFileName = ConstantConfig.CONFIG_FILE_NAME;
-            URL configUrl = ParallelOkPerf.class.getClassLoader().getResource(configFileName);
-            if (configUrl == null) {
-                System.out.println("The configFile " + configFileName + " doesn't exist!");
-                return;
-            }
+	public static void main(String[] args) throws ContractException, IOException, InterruptedException {
+		try {
+			String configFileName = ConstantConfig.CONFIG_FILE_NAME;
+			URL configUrl = ParallelOkPerf.class.getClassLoader().getResource(configFileName);
+			if (configUrl == null) {
+				System.out.println("The configFile " + configFileName + " doesn't exist!");
+				return;
+			}
 
-            if (args.length < 4) {
-                Usage();
-                return;
-            }
-            String groupId = args[0];
-            int userCount = Integer.valueOf(args[1]).intValue();
-            Integer count = Integer.valueOf(args[2]).intValue();
-            Integer qps = Integer.valueOf(args[3]).intValue();
+			if (args.length < 4) {
+				usage();
+				return;
+			}
+			String groupId = args[0];
+			int userCount = Integer.valueOf(args[1]).intValue();
+			Integer count = Integer.valueOf(args[2]).intValue();
+			Integer qps = Integer.valueOf(args[3]).intValue();
 
-            String configFile = configUrl.getPath();
-            BcosSDK sdk = BcosSDK.build(configFile);
-            client = sdk.getClient(groupId);
-            ThreadPoolService threadPoolService =
-                    new ThreadPoolService("DMCClient", Runtime.getRuntime().availableProcessors());
+			String configFile = configUrl.getPath();
+			BcosSDK sdk = BcosSDK.build(configFile);
+			client = sdk.getClient(groupId);
+			ThreadPoolService threadPoolService = new ThreadPoolService("DMCClient",
+					Runtime.getRuntime().availableProcessors());
 
-            start(groupId, userCount, count, qps, threadPoolService);
+			start(groupId, userCount, count, qps, threadPoolService);
 
-            threadPoolService.getThreadPool().awaitTermination(0, TimeUnit.SECONDS);
-            System.exit(0);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(0);
-        }
-    }
+			threadPoolService.getThreadPool().awaitTermination(0, TimeUnit.SECONDS);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
 
-    public static void start(
-            String groupId,
-            int userCount,
-            int count,
-            Integer qps,
-            ThreadPoolService threadPoolService)
-            throws IOException, InterruptedException, ContractException {
-        System.out.println(
-                "====== Start test, user count: "
-                        + userCount
-                        + ", count: "
-                        + count
-                        + ", qps:"
-                        + qps
-                        + ", groupId: "
-                        + groupId);
+	public static void start(String groupId, int userCount, int count, Integer qps, ThreadPoolService threadPoolService)
+			throws IOException, InterruptedException, ContractException {
+		System.out.println("====== Start test, user count: " + userCount + ", count: " + count + ", qps:" + qps
+				+ ", groupId: " + groupId);
 
-        RateLimiter limiter = RateLimiter.create(qps.intValue());
+		RateLimiter limiter = RateLimiter.create(qps.intValue());
 
-        Account[] accounts = new Account[userCount];
-        Map<Integer, AtomicLong> summary = new ConcurrentHashMap<Integer, AtomicLong>();
+		Account[] accounts = new Account[userCount];
+		AtomicLong[] summary = new AtomicLong[userCount];
 
-        final Random random = new Random();
-        random.setSeed(System.currentTimeMillis());
+		final Random random = new Random();
+		random.setSeed(System.currentTimeMillis());
 
-        System.out.println("Create account...");
-        CountDownLatch userLatch = new CountDownLatch(userCount);
-        for (int i = 0; i < userCount; ++i) {
-            final int index = i;
-            threadPoolService
-                    .getThreadPool()
-                    .execute(
-                            new Runnable() {
-                                public void run() {
-                                    Account account;
-                                    try {
-                                        long initBalance = Math.abs(random.nextLong());
+		System.out.println("Create account...");
+		IntStream.range(0, userCount).parallel().forEach(i -> {
+			Account account;
+			try {
+				long initBalance = Math.abs(random.nextLong());
 
-                                        limiter.acquire();
-                                        account =
-                                                Account.deploy(
-                                                        client,
-                                                        client.getCryptoSuite().getCryptoKeyPair());
-                                        account.addBalance(BigInteger.valueOf(initBalance));
+				limiter.acquire();
+				account = Account.deploy(client, client.getCryptoSuite().getCryptoKeyPair());
+				account.addBalance(BigInteger.valueOf(initBalance));
 
-                                        accounts[index] = account;
-                                        summary.put(index, new AtomicLong(initBalance));
-                                        userLatch.countDown();
-                                    } catch (ContractException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            });
-        }
-        userLatch.await();
-        System.out.println("Create account finished!");
+				accounts[i] = account;
+				summary[i] = new AtomicLong(initBalance);
+			} catch (ContractException e) {
+				e.printStackTrace();
+			}
+		});
+		System.out.println("Create account finished!");
 
-        System.out.println("Sending transactions...");
-        ProgressBar sendedBar =
-                new ProgressBarBuilder()
-                        .setTaskName("Send   :")
-                        .setInitialMax(count)
-                        .setStyle(ProgressBarStyle.UNICODE_BLOCK)
-                        .build();
-        ProgressBar receivedBar =
-                new ProgressBarBuilder()
-                        .setTaskName("Receive:")
-                        .setInitialMax(count)
-                        .setStyle(ProgressBarStyle.UNICODE_BLOCK)
-                        .build();
+		System.out.println("Sending transactions...");
+		ProgressBar sendedBar = new ProgressBarBuilder().setTaskName("Send   :").setInitialMax(count)
+				.setStyle(ProgressBarStyle.UNICODE_BLOCK).build();
+		ProgressBar receivedBar = new ProgressBarBuilder().setTaskName("Receive:").setInitialMax(count)
+				.setStyle(ProgressBarStyle.UNICODE_BLOCK).build();
 
-        CountDownLatch transactionLatch = new CountDownLatch(count);
-        AtomicLong totalCost = new AtomicLong(0);
-        Collector collector = new Collector();
-        collector.setTotal(count);
+		CountDownLatch transactionLatch = new CountDownLatch(count);
+		AtomicLong totalCost = new AtomicLong(0);
+		Collector collector = new Collector();
+		collector.setTotal(count);
 
-        for (int i = 0; i < count; ++i) {
-            limiter.acquire();
+		IntStream.range(0, count).parallel().forEach(i -> {
+			limiter.acquire();
 
-            final int index = i % accounts.length;
-            threadPoolService
-                    .getThreadPool()
-                    .execute(
-                            new Runnable() {
-                                @Override
-                                public void run() {
-                                    Account account = accounts[index];
-                                    long now = System.currentTimeMillis();
+			final int index = i % accounts.length;
+			Account account = accounts[index];
+			long now = System.currentTimeMillis();
 
-                                    final long value = Math.abs(random.nextLong() % 1000);
+			final long value = Math.abs(random.nextLong() % 1000);
 
-                                    account.addBalance(
-                                            BigInteger.valueOf(value),
-                                            new TransactionCallback() {
-                                                @Override
-                                                public void onResponse(TransactionReceipt receipt) {
-                                                    AtomicLong count = summary.get(index);
-                                                    count.addAndGet(value);
+			account.addBalance(BigInteger.valueOf(value), new TransactionCallback() {
+				@Override
+				public void onResponse(TransactionReceipt receipt) {
+					AtomicLong count = summary[index];
+					count.addAndGet(value);
 
-                                                    long cost = System.currentTimeMillis() - now;
-                                                    collector.onMessage(receipt, cost);
+					long cost = System.currentTimeMillis() - now;
+					collector.onMessage(receipt, cost);
 
-                                                    receivedBar.step();
-                                                    transactionLatch.countDown();
-                                                    totalCost.addAndGet(
-                                                            System.currentTimeMillis() - now);
-                                                }
-                                            });
-                                    sendedBar.step();
-                                }
-                            });
-        }
-        transactionLatch.await();
+					receivedBar.step();
+					transactionLatch.countDown();
+					totalCost.addAndGet(System.currentTimeMillis() - now);
+				}
+			});
+			sendedBar.step();
+		});
+		transactionLatch.await();
 
-        sendedBar.close();
-        receivedBar.close();
-        collector.report();
+		sendedBar.close();
+		receivedBar.close();
+		collector.report();
+		System.out.println("Sending transactions finished!");
 
-        System.out.println("Sending transactions finished!");
+		System.out.println("Checking result...");
+		IntStream.range(0, summary.length).parallel().forEach(i -> {
+			limiter.acquire();
+			final long expectBalance = summary[i].longValue();
+			try {
+				limiter.acquire();
+				BigInteger balance = accounts[i].balance();
+				if (balance.longValue() != expectBalance) {
+					System.out.println("Check failed! Account[" + i + "] balance: " + balance
+							+ " not equal to expected: " + expectBalance);
+				}
+			} catch (ContractException e) {
+				e.printStackTrace();
+			}
+		});
 
-        System.out.println("Checking result...");
-        CountDownLatch checkLatch = new CountDownLatch(count);
-        for (Map.Entry<Integer, AtomicLong> entry : summary.entrySet()) {
-            limiter.acquire();
-            final int index = entry.getKey().intValue();
-            final long expectBalance = entry.getValue().longValue();
-            threadPoolService
-                    .getThreadPool()
-                    .execute(
-                            new Runnable() {
-                                @Override
-                                public void run() {
-                                    try {
-                                        limiter.acquire();
-                                        BigInteger balance = accounts[index].balance();
-                                        if (balance.longValue() != expectBalance) {
-                                            System.out.println(
-                                                    "Check failed! Account["
-                                                            + index
-                                                            + "] balance: "
-                                                            + balance
-                                                            + " not equal to expected: "
-                                                            + expectBalance);
-                                        }
-
-                                        checkLatch.countDown();
-                                    } catch (ContractException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            });
-        }
-        System.out.println("Checking finished!");
-
-        // collector.
-        // System.out.println("Total elapsed: " + elapsed);
-        // System.out.println("TPS: " + (double) count / ((double) elapsed / 1000));
-    }
+		System.out.println("Checking finished!");
+	}
 }
