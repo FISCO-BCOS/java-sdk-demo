08f1d8696aefd56ac75cd8964563f506028d636a
wenlinli <1574249665@qq.com>
2024-06-26 14:17:30 +0800

    add some test sol for release-3.10.0

---
+++ src/main/java/org/fisco/bcos/sdk/demo/contract/sol/MainContract.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.25;
+
+import "./StorageSlot.sol";
+import "./ContractA.sol";
+import "./ContractB.sol";
+
+contract MainContract {
+
+    function checkAndVerifyIntValue(int256 value) public returns (bool) {
+        ContractA a = new ContractA(value);
+        int256 result = a.callContractB();
+        require(result == value, "store value not equal tload result");
+        return true;
+    }
+}
